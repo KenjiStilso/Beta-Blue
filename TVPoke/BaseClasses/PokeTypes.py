2a7f75faea6dcfaf9c0d634651694009cf93ba13
bridget-gahr <gahrbridget@gmail.com>
2026-04-15 11:39:33 -0700

    Types

--- TVPoke/BaseClasses/PokeTypes.py
+++ TVPoke/BaseClasses/PokeTypes.py
@@ -1,60 +1,94 @@
 from TVPoke.BaseClasses.PokeParentClass import Pokemon
 
-class Water(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "WATER", "ELECTRIC", moves, imgPath)
-
-class Ground(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "GROUND", "GRASS", moves, imgPath) 
-
-class Grass(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "GRASS", "FIRE", moves, imgPath) 
-
-class Fire(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "FIRE", "WATER", moves, imgPath)
-
-class Electric(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "ELECTRIC", "GROUND", moves, imgPath)
-
-class Normal(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "NORMAL", "FIGHT", moves, imgPath)
-class Fighting(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "FIGHTING", "PSYCIC", moves, imgPath)
-class Psychic(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "PSYCHIC", "DARK", moves, imgPath)
-
-class Ghost(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "GHOST", "GHOST", moves, imgPath)
-        
-class Bug(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "BUG", "FIRE", moves, imgPath)
-
-class Poison(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "POISON", "GROUND", moves, imgPath)
-        
-class Dark(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "DARK", "BUG", moves, imgPath)
-
-class Ice(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "ICE", "FIRE", moves, imgPath)
-
-class Dragon(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "DRAGON", "DRAGON", moves, imgPath)
-
-class Flying(Pokemon):
-    def __init__(self, name, hp, moves, imgPath):
-        super().__init__(name, hp, "FLYING", "ELECTRIC", moves, imgPath)
-
+TYPE_CHART = {
+    "NORMAL": {
+        "weak": ["FIGHTING"],
+        "resist": [],
+        "immune": ["GHOST"]
+    },
+    "FIRE": {
+        "weak": ["WATER", "GROUND", "ROCK"],
+        "resist": ["FIRE", "GRASS", "ICE", "BUG", "STEEL", "FAIRY"],
+        "immune": []
+    },
+    "WATER": {
+        "weak": ["ELECTRIC", "GRASS"],
+        "resist": ["FIRE", "WATER", "ICE", "STEEL"],
+        "immune": []
+    },
+    "ELECTRIC": {
+        "weak": ["GROUND"],
+        "resist": ["ELECTRIC", "FLYING", "STEEL"],
+        "immune": []
+    },
+    "GRASS": {
+        "weak": ["FIRE", "ICE", "POISON", "FLYING", "BUG"],
+        "resist": ["WATER", "ELECTRIC", "GRASS", "GROUND"],
+        "immune": []
+    },
+    "ICE": {
+        "weak": ["FIRE", "FIGHTING", "ROCK", "STEEL"],
+        "resist": ["ICE"],
+        "immune": []
+    },
+    "FIGHTING": {
+        "weak": ["FLYING", "PSYCHIC", "FAIRY"],
+        "resist": ["BUG", "ROCK", "DARK"],
+        "immune": []
+    },
+    "POISON": {
+        "weak": ["GROUND", "PSYCHIC"],
+        "resist": ["GRASS", "FIGHTING", "POISON", "BUG", "FAIRY"],
+        "immune": []
+    },
+    "GROUND": {
+        "weak": ["WATER", "GRASS", "ICE"],
+        "resist": ["POISON", "ROCK"],
+        "immune": ["ELECTRIC"]
+    },
+    "FLYING": {
+        "weak": ["ELECTRIC", "ICE", "ROCK"],
+        "resist": ["GRASS", "FIGHTING", "BUG"],
+        "immune": ["GROUND"]
+    },
+    "PSYCHIC": {
+        "weak": ["BUG", "GHOST", "DARK"],
+        "resist": ["FIGHTING", "PSYCHIC"],
+        "immune": []
+    },
+    "BUG": {
+        "weak": ["FIRE", "FLYING", "ROCK"],
+        "resist": ["GRASS", "FIGHTING", "GROUND"],
+        "immune": []
+    },
+    "ROCK": {
+        "weak": ["WATER", "GRASS", "FIGHTING", "GROUND", "STEEL"],
+        "resist": ["NORMAL", "FIRE", "POISON", "FLYING"],
+        "immune": []
+    },
+    "GHOST": {
+        "weak": ["GHOST", "DARK"],
+        "resist": ["POISON", "BUG"],
+        "immune": ["NORMAL", "FIGHTING"]
+    },
+    "DRAGON": {
+        "weak": ["ICE", "DRAGON", "FAIRY"],
+        "resist": ["FIRE", "WATER", "ELECTRIC", "GRASS"],
+        "immune": []
+    },
+    "DARK": {
+        "weak": ["FIGHTING", "BUG", "FAIRY"],
+        "resist": ["GHOST", "DARK"],
+        "immune": ["PSYCHIC"]
+    },
+    "STEEL": {
+        "weak": ["FIRE", "FIGHTING", "GROUND"],
+        "resist": ["NORMAL", "GRASS", "ICE", "FLYING", "PSYCHIC", "BUG", "ROCK", "DRAGON", "STEEL", "FAIRY"],
+        "immune": ["POISON"]
+    },
+    "FAIRY": {
+        "weak": ["POISON", "STEEL"],
+        "resist": ["FIGHTING", "BUG", "DARK"],
+        "immune": ["DRAGON"]
+    }
+}
