5826ae46cc8b9b34ba59b0139cddd0f373d12863
bridget-gahr <gahrbridget@gmail.com>
2026-04-17 11:28:47 -0700

    start screen

---
+++ run_game.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+tvpoke_root = os.path.join(this_dir, "TVPoke")
+if tvpoke_root not in sys.path:
+    sys.path.insert(0, tvpoke_root)
+
+from PyUI.Window import Window
+from PyUI.Examples.StartScreen import StartScreen
+import pygame
+
+
+def main():
+    window = Window("Beta-Blue", (120, 184, 255))
+    start = StartScreen(window)
+    screen = start
+    clock = pygame.time.Clock()
+
+    while True:
+        window.checkForInput(screen)
+        window.update(screen)
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
